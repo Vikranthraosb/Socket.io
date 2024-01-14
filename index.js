12904cb59ef8f0090cbd87ab411c657a4ee9cce5
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-01-14 23:35:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set("view engine", "ejs");
+
+// Socket.io
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+   res.render("index");
+});
+app.get("/messege2", (req, res) => {
+  res.render("messege2");
+});
+
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
